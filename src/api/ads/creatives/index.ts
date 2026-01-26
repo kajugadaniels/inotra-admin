50173461b1fe262ceaee2d0f94280f9f5e17be32
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-01-27 00:08:48 +0200

    Add ads creatives API barrel exports

---
+++ src/api/ads/creatives/index.ts
@@ -0,0 +1,5 @@
+export { listAdCreatives } from "./listAdCreatives";
+export { getAdCreative } from "./getAdCreative";
+export { createAdCreative } from "./createAdCreative";
+export { updateAdCreative } from "./updateAdCreative";
+export { deleteAdCreative } from "./deleteAdCreative";
