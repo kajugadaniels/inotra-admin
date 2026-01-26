ca3cae7eb918af61b947c97dbfd5a93fa683e228
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-01-26 19:30:52 +0200

    Add admin users component exports

---
+++ src/components/shared/users/index.ts
@@ -0,0 +1,5 @@
+export { default as UsersHeader } from "./UsersHeader";
+export { default as UsersFilters } from "./UsersFilters";
+export type { UsersFiltersState } from "./UsersFilters";
+export { default as UsersTable } from "./UsersTable";
+export { default as UsersPagination } from "./UsersPagination";
