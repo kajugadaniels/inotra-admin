4d91538ff87edde6c7492461529617257405448f
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-01-26 11:39:08 +0200

    Add ThemeProvider component

---
+++ src/components/shared/theme-provider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import type { ReactNode } from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+type ThemeProviderProps = {
+    children: ReactNode;
+};
+
+const ThemeProvider = ({ children }: ThemeProviderProps) => {
+    return (
+        <NextThemesProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="peak-admin-theme"
+            disableTransitionOnChange
+        >
+            {children}
+        </NextThemesProvider>
+    );
+};
+
+export default ThemeProvider;
