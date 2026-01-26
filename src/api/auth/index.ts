61ca32f29dcf2165684f424d9bbce172b171dfd7
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-01-26 11:34:24 +0200

    Add auth API exports for admin

---
+++ src/api/auth/index.ts
@@ -0,0 +1,6 @@
+export { loginAdminWithPassword } from "./login";
+export { exchangeAdminGoogleToken } from "./googleLogin";
+export { requestAdminPasswordReset } from "./passwordResetRequest";
+export { resendAdminPasswordResetOtp } from "./passwordResetResend";
+export { confirmAdminPasswordReset } from "./passwordResetConfirm";
+export { updateAdminProfile } from "./meUpdate";
