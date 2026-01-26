b1da59e49c7e9eeb264b18268cbefddcd623ce99
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-01-26 22:56:17 +0200

    Export ad placements page component

--- src/components/shared/ads/placements/index.ts
+++ src/components/shared/ads/placements/index.ts
@@ -3,3 +3,4 @@ export { default as AdPlacementsFilters } from "./AdPlacementsFilters";
 export type { AdPlacementsFiltersState } from "./AdPlacementsFilters";
 export { default as AdPlacementsTable } from "./AdPlacementsTable";
 export { default as AdPlacementsPagination } from "./AdPlacementsPagination";
+export { default as AdPlacementsPage } from "./AdPlacementsPage";
