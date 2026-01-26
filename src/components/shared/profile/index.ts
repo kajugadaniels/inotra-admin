4c8f647ab285988447fe720bd220685e482d8e23
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-01-26 18:55:56 +0200

    Export admin profile components

---
+++ src/components/shared/profile/index.ts
@@ -0,0 +1,4 @@
+export { default as AdminProfileHeader } from "./AdminProfileHeader";
+export { default as AdminProfileTabs } from "./AdminProfileTabs";
+export { default as AdminProfileDetailsForm } from "./AdminProfileDetailsForm";
+export { default as AdminPasswordForm } from "./AdminPasswordForm";
