c641d10317f64f1164edf58cc8dd0af7f7a798de
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-01-26 22:42:54 +0200

    Add API client to update ad placements

---
+++ src/api/ads/placements/updateAdPlacement.ts
@@ -0,0 +1,23 @@
+import { requestJson } from "../../http";
+import type { AdPlacement } from "../../types";
+
+type Args = {
+    apiBaseUrl: string;
+    accessToken: string;
+    placementId: string;
+    data: {
+        key?: string;
+        title?: string;
+        is_active?: boolean;
+    };
+};
+
+export function updateAdPlacement({ apiBaseUrl, accessToken, placementId, data }: Args) {
+    return requestJson<{ message: string; placement: AdPlacement }>({
+        apiBaseUrl,
+        path: `/ads/placements/${placementId}/update/`,
+        method: "PATCH",
+        accessToken,
+        body: data,
+    });
+}
