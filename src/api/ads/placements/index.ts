8d493aa4ca276a5ab1324b12c8137bac8b2b77af
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-01-26 22:43:03 +0200

    Add placements API barrel exports

---
+++ src/api/ads/placements/index.ts
@@ -0,0 +1,5 @@
+export { listAdPlacements } from "./listAdPlacements";
+export { createAdPlacement } from "./createAdPlacement";
+export { updateAdPlacement } from "./updateAdPlacement";
+export { deleteAdPlacement } from "./deleteAdPlacement";
+export { getAdPlacement } from "./getAdPlacement";
