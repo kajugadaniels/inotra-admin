91285b88d90575d1c6462b30f3b16ee7f57771b8
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-01-26 12:38:37 +0200

    Add admin auth component exports

---
+++ src/components/shared/auth/index.ts
@@ -0,0 +1,7 @@
+export { default as AdminHeroPanel } from "./AdminHeroPanel";
+export { default as AdminMobileHero } from "./AdminMobileHero";
+export { default as AdminLoginHeader } from "./AdminLoginHeader";
+export { default as AdminCredentialsForm } from "./AdminCredentialsForm";
+export type { AdminCredentialsFormProps } from "./AdminCredentialsForm";
+export { default as AdminGoogleSignIn } from "./AdminGoogleSignIn";
+export type { AdminGoogleSignInProps } from "./AdminGoogleSignIn";
