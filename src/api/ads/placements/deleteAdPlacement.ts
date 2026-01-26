8da5c93326e6fa1432574cbf9a2146163dcffdd0
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-01-26 22:42:57 +0200

    Add API client to delete ad placements

---
+++ src/api/ads/placements/deleteAdPlacement.ts
@@ -0,0 +1,17 @@
+import { requestJson } from "../../http";
+import type { BasicMessageResponse } from "../../types";
+
+type Args = {
+    apiBaseUrl: string;
+    accessToken: string;
+    placementId: string;
+};
+
+export function deleteAdPlacement({ apiBaseUrl, accessToken, placementId }: Args) {
+    return requestJson<BasicMessageResponse>({
+        apiBaseUrl,
+        path: `/ads/placements/${placementId}/delete/`,
+        method: "DELETE",
+        accessToken,
+    });
+}
