a62f7d0bce3cf8a467688ea87c78745f2bc4109c
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-01-26 22:42:51 +0200

    Add API client to create ad placements

---
+++ src/api/ads/placements/createAdPlacement.ts
@@ -0,0 +1,22 @@
+import { requestJson } from "../../http";
+import type { AdPlacement } from "../../types";
+
+type Args = {
+    apiBaseUrl: string;
+    accessToken: string;
+    data: {
+        key: string;
+        title?: string;
+        is_active?: boolean;
+    };
+};
+
+export function createAdPlacement({ apiBaseUrl, accessToken, data }: Args) {
+    return requestJson<{ message: string; placement: AdPlacement }>({
+        apiBaseUrl,
+        path: "/ads/placements/add/",
+        method: "POST",
+        accessToken,
+        body: data,
+    });
+}
