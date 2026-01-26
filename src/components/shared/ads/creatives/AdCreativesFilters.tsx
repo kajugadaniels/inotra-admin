2f37ed416e6197cc31bb5b53972d5865eb3b0959
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-01-27 00:19:18 +0200

    Add filters component for ad creatives page

--- src/components/shared/ads/creatives/AdCreativesFilters.tsx
+++ src/components/shared/ads/creatives/AdCreativesFilters.tsx
@@ -166,12 +166,14 @@ const AdCreativesFilters = ({
                                 <Button
                                     type="button"
                                     variant="outline"
+                                    className="h-11 rounded-full"
                                     onClick={() => setDialogOpen(false)}
                                 >
                                     Cancel
                                 </Button>
                                 <Button
                                     type="button"
+                                    className="h-11 rounded-full"
                                     onClick={() => {
                                         onFiltersChange(draftFilters);
                                         setDialogOpen(false);
