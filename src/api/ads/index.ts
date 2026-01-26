792942a9bf236cf475f589544a684c02b9566d92
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-01-26 22:43:07 +0200

    Add ads API exports

---
+++ src/api/ads/index.ts
@@ -0,0 +1,5 @@
+export { listAdPlacements } from "./placements/listAdPlacements";
+export { createAdPlacement } from "./placements/createAdPlacement";
+export { updateAdPlacement } from "./placements/updateAdPlacement";
+export { deleteAdPlacement } from "./placements/deleteAdPlacement";
+export { getAdPlacement } from "./placements/getAdPlacement";
