c993207a0bc99f9ace26ecbdff96dd970a0c528c
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-01-28 19:16:19 +0200

    Set admin theme default to light with localStorage persistence

--- src/components/shared/theme-provider.tsx
+++ src/components/shared/theme-provider.tsx
@@ -11,7 +11,7 @@ const ThemeProvider = ({ children }: ThemeProviderProps) => {
     return (
         <NextThemesProvider
             attribute="class"
-            defaultTheme="dark"
+            defaultTheme="light"
             enableSystem={false}
             storageKey="inotra-theme"
             disableTransitionOnChange
