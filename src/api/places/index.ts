7b592e1f8dd25342cf20bdf3756c55062b884613
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-01-29 01:45:33 +0200

    Add place API barrel exports

---
+++ src/api/places/index.ts
@@ -0,0 +1,10 @@
+export { listPlaceCategories } from "./categories/listPlaceCategories";
+export { createPlaceCategory } from "./categories/createPlaceCategory";
+export { updatePlaceCategory } from "./categories/updatePlaceCategory";
+export { deletePlaceCategory } from "./categories/deletePlaceCategory";
+
+export { listPlaces } from "./listPlaces";
+export { getPlace } from "./getPlace";
+export { createPlace } from "./createPlace";
+export { updatePlace } from "./updatePlace";
+export { deletePlace } from "./deletePlace";
