5b3bc9253ef17e0f130cbf4affaaf56483a8aa89
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-01-29 02:23:56 +0200

    Export listings components

---
+++ src/components/shared/listings/index.ts
@@ -0,0 +1,9 @@
+export { default as ListingHeader } from "./ListingHeader";
+export { default as ListingFilters } from "./ListingFilters";
+export type { ListingFiltersState } from "./ListingFilters";
+export { default as ListingTable } from "./ListingTable";
+export { default as ListingPagination } from "./ListingPagination";
+export { default as ListingDetailsDialog } from "./ListingDetailsDialog";
+export { default as ListingDeleteDialog } from "./ListingDeleteDialog";
+export { default as ListingForm } from "./ListingForm";
+export type { ListingFormState } from "./ListingForm";
