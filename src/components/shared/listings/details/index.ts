6b3cf3828cd4ca295edfb5ed80d5bba8a51845ad
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-01-30 11:09:09 +0200

    Export listing details components

---
+++ src/components/shared/listings/details/index.ts
@@ -0,0 +1,6 @@
+export { default as ListingDetailsSidebar } from "./ListingDetailsSidebar";
+export { default as ListingOverview } from "./ListingOverview";
+export { default as ListingLocationDetails } from "./ListingLocationDetails";
+export { default as ListingContactDetails } from "./ListingContactDetails";
+export { default as ListingOperationsDetails } from "./ListingOperationsDetails";
+export { default as ListingMediaDetails } from "./ListingMediaDetails";
