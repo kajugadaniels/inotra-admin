63976ba8dbeec44e7e5db3985950f3e192c8d8dd
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-01-31 20:15:10 +0200

    Remove listing media details export

--- src/components/shared/listings/details/index.ts
+++ src/components/shared/listings/details/index.ts
@@ -3,4 +3,3 @@ export { default as ListingOverview } from "./ListingOverview";
 export { default as ListingLocationDetails } from "./ListingLocationDetails";
 export { default as ListingContactDetails } from "./ListingContactDetails";
 export { default as ListingOperationsDetails } from "./ListingOperationsDetails";
-export { default as ListingMediaDetails } from "./ListingMediaDetails";
