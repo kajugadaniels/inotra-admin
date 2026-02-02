709c6af2bf13ae5c0f4aba35700044988cde68c5
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-02 21:48:18 +0200

    Export defaultUsersFilters for reuse across user pages

--- src/components/shared/users/index.ts
+++ src/components/shared/users/index.ts
@@ -1,6 +1,7 @@
 export { default as UsersHeader } from "./UsersHeader";
 export { default as UsersFilters } from "./UsersFilters";
 export type { UsersFiltersState } from "./UsersFilters";
+export { defaultUsersFilters } from "./UsersFilters";
 export { default as UsersTable } from "./UsersTable";
 export { default as UsersPagination } from "./UsersPagination";
 export { default as UserDetailsSheet } from "./UserDetailsSheet";
