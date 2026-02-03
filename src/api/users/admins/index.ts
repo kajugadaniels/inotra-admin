7ee87466c876ecaa53099584c0f1b802416e7423
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-03 14:02:11 +0200

    Export admin user API clients

---
+++ src/api/users/admins/index.ts
@@ -0,0 +1,5 @@
+export { listAdmins } from "./listAdmins";
+export { createAdmin } from "./createAdmin";
+export { activateAdmin } from "./activateAdmin";
+export { toggleAdminActive } from "./toggleAdminActive";
+export { deleteAdmin } from "./deleteAdmin";
