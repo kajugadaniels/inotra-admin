d484cff5e6720842d46143b47b7d5bbeaa756ce5
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-03 16:08:23 +0200

    Export event UI components via barrel

---
+++ src/components/events/index.ts
@@ -0,0 +1,6 @@
+export { default as EventHeader } from "./EventHeader";
+export { default as EventFilters, defaultEventFilters } from "./EventFilters";
+export type { EventFiltersState } from "./EventFilters";
+export { default as EventGrid } from "./EventGrid";
+export { default as EventCard } from "./EventCard";
+export { default as EventPagination } from "./EventPagination";
