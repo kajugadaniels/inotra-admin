e8e86833f49d1822aafa655f9af0f4f5535401eb
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-03 15:57:32 +0200

    Expose event API clients via events barrel

---
+++ src/api/events/index.ts
@@ -0,0 +1,7 @@
+export { listEvents } from "./listEvents";
+export type { EventListItem } from "./listEvents";
+export { getEvent } from "./getEvent";
+export type { EventDetail } from "./getEvent";
+export { createEvent } from "./createEvent";
+export { updateEvent } from "./updateEvent";
+export { deleteEvent } from "./deleteEvent";
