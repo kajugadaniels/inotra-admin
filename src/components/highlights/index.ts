482543ad3c9739c61b93a858fb528c663a06fcda
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-03 19:21:11 +0200

    Export highlight UI components from barrel file

---
+++ src/components/highlights/index.ts
@@ -0,0 +1,4 @@
+export { default as HighlightCard } from "./HighlightCard";
+export { default as HighlightGrid } from "./HighlightGrid";
+export { default as HighlightPagination } from "./HighlightPagination";
+export { default as HighlightDeleteDialog } from "./HighlightDeleteDialog";
