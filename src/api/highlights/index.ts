81536cf11d6682d7fc297382406da6cd5f334061
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-03 18:46:57 +0200

    Expose highlight API clients via barrel

---
+++ src/api/highlights/index.ts
@@ -0,0 +1,6 @@
+export { listHighlights } from "./listHighlights";
+export type { Highlight, HighlightMedia } from "./listHighlights";
+export { getHighlight } from "./getHighlight";
+export { createHighlight } from "./createHighlight";
+export { updateHighlight } from "./updateHighlight";
+export { deleteHighlight } from "./deleteHighlight";
