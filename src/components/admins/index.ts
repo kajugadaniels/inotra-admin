ed6687b7ee390a5a310c0a277a3c8a17bba7d72f
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-03 14:15:30 +0200

    Expose admin components via index barrel

---
+++ src/components/admins/index.ts
@@ -0,0 +1,9 @@
+export { default as AdminHeader } from "./AdminHeader";
+export { default as AdminFilters, defaultAdminFilters } from "./AdminFilters";
+export type { AdminFiltersState } from "./AdminFilters";
+export { default as AdminTable } from "./AdminTable";
+export { default as AdminPagination } from "./AdminPagination";
+export { default as AdminDetailsSheet } from "./AdminDetailsSheet";
+export { default as AdminDeleteDialog } from "./AdminDeleteDialog";
+export { default as AdminCreateDialog } from "./AdminCreateDialog";
+export type { AdminForm } from "./AdminCreateDialog";
