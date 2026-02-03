65e53cc73c16d3e70bc341332b3290bf61971a00
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-03 10:06:31 +0200

    Export customer representative API clients

---
+++ src/api/users/customer-reps/index.ts
@@ -0,0 +1,5 @@
+export { listCustomerReps } from "./listCustomerReps";
+export { createCustomerRep } from "./createCustomerRep";
+export { activateCustomerRep } from "./activateCustomerRep";
+export { toggleCustomerRepActive } from "./toggleCustomerRepActive";
+export { deleteCustomerRep } from "./deleteCustomerRep";
