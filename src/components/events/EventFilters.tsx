ca07f2b194be04af339081b007569b6cc21c2a6d
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-03 16:17:34 +0200

    Add event filters dialog component

--- src/components/events/EventFilters.tsx
+++ src/components/events/EventFilters.tsx
@@ -21,7 +21,6 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select";
-import { DatePicker } from "@/components/ui/date-picker";
 
 export type EventFiltersState = {
     search: string;
