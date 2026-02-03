04bc70007b21b5537fe3ad1adfa3395ccc3961be
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-03 14:41:48 +0200

    Export admin UI components without details sheet

--- src/components/admins/index.ts
+++ src/components/admins/index.ts
@@ -3,7 +3,6 @@ export { default as AdminFilters, defaultAdminFilters } from "./AdminFilters";
 export type { AdminFiltersState } from "./AdminFilters";
 export { default as AdminTable } from "./AdminTable";
 export { default as AdminPagination } from "./AdminPagination";
-export { default as AdminDetailsSheet } from "./AdminDetailsSheet";
 export { default as AdminDeleteDialog } from "./AdminDeleteDialog";
 export { default as AdminCreateDialog } from "./AdminCreateDialog";
 export type { AdminForm } from "./AdminCreateDialog";
