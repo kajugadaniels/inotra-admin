3a56ec165669e51c9d40142334d67fabea30a4ba
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-03 14:42:33 +0200

    Remove user details sheet export

--- src/components/shared/users/index.ts
+++ src/components/shared/users/index.ts
@@ -4,5 +4,4 @@ export type { UsersFiltersState } from "./UsersFilters";
 export { defaultUsersFilters } from "./UsersFilters";
 export { default as UsersTable } from "./UsersTable";
 export { default as UsersPagination } from "./UsersPagination";
-export { default as UserDetailsSheet } from "./UserDetails";
 export { default as UserDeleteDialog } from "./UserDeleteDialog";
