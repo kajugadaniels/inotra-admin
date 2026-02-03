cc96b1d970de8c8124fac354b587260ee643447a
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-03 10:51:28 +0200

    Export customer rep component suite

---
+++ src/components/customer-reps/index.ts
@@ -0,0 +1,8 @@
+export { default as CustomerRepHeader } from "./CustomerRepHeader";
+export { default as CustomerRepFilters } from "./CustomerRepFilters";
+export type { CustomerRepFiltersState } from "./CustomerRepFilters";
+export { defaultCustomerRepFilters } from "./CustomerRepFilters";
+export { default as CustomerRepTable } from "./CustomerRepTable";
+export { default as CustomerRepPagination } from "./CustomerRepPagination";
+export { default as CustomerRepDetailsSheet } from "./CustomerRepDetailsSheet";
+export { default as CustomerRepDeleteDialog } from "./CustomerRepDeleteDialog";
