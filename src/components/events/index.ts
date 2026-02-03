e96df034277b1a7b805de8e2c19c78f19af59f91
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-03 16:26:23 +0200

    Export new event form and dialogs via barrel

--- src/components/events/index.ts
+++ src/components/events/index.ts
@@ -5,3 +5,5 @@ export { default as EventGrid } from "./EventGrid";
 export { default as EventCard } from "./EventCard";
 export { default as EventPagination } from "./EventPagination";
 export { default as EventDeleteDialog } from "./EventDeleteDialog";
+export { default as EventForm, defaultEventForm } from "./EventForm";
+export { default as EventFormSidebar } from "./EventFormSidebar";
