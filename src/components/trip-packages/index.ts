5509aea84dc0dd792fc2e4ba431c956412bde19d
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-04 05:17:26 +0200

    Update trip packages exports for table and package form

--- src/components/trip-packages/index.ts
+++ src/components/trip-packages/index.ts
@@ -1,6 +1,7 @@
 export { default as TripPackagesHeader } from "./TripPackagesHeader";
 export { default as TripPackagesFilters } from "./TripPackagesFilters";
 export type { TripPackagesFiltersState } from "./TripPackagesFilters";
-export { default as TripPackagesGrid } from "./TripPackagesGrid";
-export { default as TripPackageCard } from "./TripPackageCard";
+export { default as TripPackagesTable } from "./TripPackagesTable";
+export { default as TripPackageDeleteDialog } from "./TripPackageDeleteDialog";
 
+export * from "./form";
