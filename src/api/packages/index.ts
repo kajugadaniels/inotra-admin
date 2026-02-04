f9c17e754e71b31151b2ebc71e99ab49c50f5d19
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-04 04:32:26 +0200

    Export packages API helpers from packages module

---
+++ src/api/packages/index.ts
@@ -0,0 +1,15 @@
+export { listPackages } from "./listPackages";
+export type { PackageListItem, ListPackagesArgs } from "./listPackages";
+
+export { getPackage } from "./getPackage";
+export type { PackageDetail, PackageImage, PackageActivity } from "./getPackage";
+
+export { createPackage } from "./createPackage";
+export type { CreatePackageInput, PackageActivityInput } from "./createPackage";
+
+export { updatePackage } from "./updatePackage";
+export type { UpdatePackageInput } from "./updatePackage";
+
+export { togglePackageActive } from "./togglePackageActive";
+export { deletePackage } from "./deletePackage";
+
