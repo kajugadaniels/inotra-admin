8557ca7704758bf7ac40f548e92d34f9d1c822ed
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-04 04:44:27 +0200

    Export trip packages components from module index

---
+++ src/components/trip-packages/index.ts
@@ -0,0 +1,6 @@
+export { default as TripPackagesHeader } from "./TripPackagesHeader";
+export { default as TripPackagesFilters } from "./TripPackagesFilters";
+export type { TripPackagesFiltersState } from "./TripPackagesFilters";
+export { default as TripPackagesGrid } from "./TripPackagesGrid";
+export { default as TripPackageCard } from "./TripPackageCard";
+
