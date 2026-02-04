3b6cf26237a0bf1f63dc715623562ae996aaf410
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-04 05:16:43 +0200

    Export trip package form components

---
+++ src/components/trip-packages/form/index.ts
@@ -0,0 +1,10 @@
+export { default as PackageForm } from "./PackageForm";
+export { default as PackageFormSidebar } from "./PackageFormSidebar";
+export { default as PackageFormBasics } from "./PackageFormBasics";
+export { default as PackageFormActivities } from "./PackageFormActivities";
+export { default as PackageFormMedia } from "./PackageFormMedia";
+export { default as PackageFormPricingStatus } from "./PackageFormPricingStatus";
+
+export type { PackageFormState, PackageActivityState, ExistingPackageImage } from "./PackageForm";
+export { defaultPackageForm } from "./PackageForm";
+
