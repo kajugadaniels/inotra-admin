6ff233753285a125103c548f30681b2d10ab70ba
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-04 07:35:53 +0200

    Add under construction page

---
+++ src/components/shared/UnderConstruction.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UnderConstruction = () => {
+    return (
+        <div>
+            Under Construction Page
+        </div>
+    )
+}
+
+export default UnderConstruction
