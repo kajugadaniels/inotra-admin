65909cfc490d6c688df64ccd2b5e88cafacfa34e
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-06 03:19:50 +0200

    Add barrel exports for highlight engagement API clients

---
+++ src/api/highlights/interactions/index.ts
@@ -0,0 +1,10 @@
+export { listHighlightLikes } from "./listHighlightLikes";
+export { listHighlightComments } from "./listHighlightComments";
+export { listHighlightShares } from "./listHighlightShares";
+export { deleteHighlightComment } from "./deleteHighlightComment";
+
+export type { ListHighlightLikesArgs } from "./listHighlightLikes";
+export type { ListHighlightCommentsArgs } from "./listHighlightComments";
+export type { ListHighlightSharesArgs } from "./listHighlightShares";
+export type { DeleteHighlightCommentResponse } from "./deleteHighlightComment";
+
