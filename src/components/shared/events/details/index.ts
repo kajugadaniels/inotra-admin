1980821a8bb3d2d1adac3457a2263d85d5e2a16c
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-06 05:46:30 +0200

    Export event details components

---
+++ src/components/shared/events/details/index.ts
@@ -0,0 +1,6 @@
+export { default as EventDetailsSidebar } from "./EventDetailsSidebar";
+export { default as EventOverview } from "./EventOverview";
+export { default as EventScheduleVenue } from "./EventScheduleVenue";
+export { default as EventPricingStatus } from "./EventPricingStatus";
+export { default as EventMedia } from "./EventMedia";
+
