0d49291a72cb6ef0b9e7fe922e2ec4f7c09e5cda
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-13 20:09:22 +0200

    Export event submission detail components

---
+++ src/components/shared/events/requests/details/index.ts
@@ -0,0 +1,5 @@
+export { default as EventSubmissionOverview } from "./EventSubmissionOverview";
+export { default as EventSubmissionScheduleVenue } from "./EventSubmissionScheduleVenue";
+export { default as EventSubmissionTickets } from "./EventSubmissionTickets";
+export { default as EventSubmissionReviewDetails } from "./EventSubmissionReviewDetails";
+
