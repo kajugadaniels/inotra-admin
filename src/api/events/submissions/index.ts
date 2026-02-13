a8aa21a628383b2151876cc559795421a036f8c6
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-13 19:50:33 +0200

    Create event submissions API barrel exports

---
+++ src/api/events/submissions/index.ts
@@ -0,0 +1,4 @@
+export { listEventSubmissions } from "./listEventSubmissions";
+export { getEventSubmission } from "./getEventSubmission";
+export { updateEventSubmissionStatus } from "./updateEventSubmissionStatus";
+
