fb338394f2db45dd8905a93d80e3a99c06bca932
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-13 19:51:52 +0200

    Export listing submission details components

---
+++ src/components/shared/listings/requests/details/index.ts
@@ -0,0 +1,6 @@
+export { default as ListingSubmissionOverview } from "./ListingSubmissionOverview";
+export { default as ListingSubmissionLocationDetails } from "./ListingSubmissionLocationDetails";
+export { default as ListingSubmissionContactDetails } from "./ListingSubmissionContactDetails";
+export { default as ListingSubmissionOperationsDetails } from "./ListingSubmissionOperationsDetails";
+export { default as ListingSubmissionReviewDetails } from "./ListingSubmissionReviewDetails";
+
