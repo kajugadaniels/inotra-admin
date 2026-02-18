ea659819f54bed1b4e51006146c722053fe79334
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-02-18 10:13:03 +0200

    Export admin chat API helpers

---
+++ src/api/chats/index.ts
@@ -0,0 +1,15 @@
+export { listChatThreads } from "./listChatThreads";
+export { listChatMessages } from "./listChatMessages";
+
+export type {
+    ChatParticipant,
+    ChatThreadListItem,
+    ListChatThreadsArgs,
+} from "./listChatThreads";
+
+export type {
+    ChatMessage,
+    ChatShareTarget,
+    ListChatMessagesArgs,
+} from "./listChatMessages";
+
